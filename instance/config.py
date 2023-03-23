112dc86270c6a3d94596bf9f3e46baa5d5014b14
Mohsin Alam <malamcsc@gmail.com>
2023-03-23 20:31:11 +0530

    code updted

--- instance/config.py
+++ instance/config.py
@@ -4,19 +4,19 @@ import os
 
 # Statement for enabling the development environment
 
-# DB_USER = os.environ.get("DB_USER","flask_user")
-# DB_PASS = os.environ.get("DB_PASS","flask")
-# #POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
-# INSTANCE_HOST = os.environ.get("INSTANCE_HOST","192.168.1.4")
-# DB_NAME = os.environ.get("DB_NAME","flaskdb")
-# DB_PORT = os.environ.get("DB_PORT","5433")
-
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_USER = os.environ.get("DB_USER","flask_user")
+DB_PASS = os.environ.get("DB_PASS","flask")
 #POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
-INSTANCE_HOST = os.environ.get("INSTANCE_HOST")
-DB_NAME = os.environ.get("DB_NAME")
-DB_PORT = os.environ.get("DB_PORT")
+INSTANCE_HOST = os.environ.get("INSTANCE_HOST","172.31.32.1")
+DB_NAME = os.environ.get("DB_NAME","flaskdb")
+DB_PORT = os.environ.get("DB_PORT","5433")
+
+# DB_USER = os.environ.get("DB_USER")
+# DB_PASS = os.environ.get("DB_PASS")
+# #POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
+# INSTANCE_HOST = os.environ.get("INSTANCE_HOST")
+# DB_NAME = os.environ.get("DB_NAME")
+# DB_PORT = os.environ.get("DB_PORT")
 
 DB_URL = 'postgresql://{user}:{pw}@{host}:{port}/{db}'.format(user=DB_USER,pw=DB_PASS,host=INSTANCE_HOST,port =DB_PORT, db=DB_NAME)
 
