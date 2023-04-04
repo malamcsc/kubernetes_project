af0f3e0f0f5c9109cc3b0918a72752c12c2720fe
malamcsc <malamcsc@gmail.com>
2023-04-04 15:15:19 +0530

    code restructure

--- instance/config.py
+++ instance/config.py
@@ -1,19 +1,7 @@
 import os
 
-# Values to be used during development. Here you might specify the URI of a database sitting on localhost.
-
-# Statement for enabling the development environment
-
-# DB_USER = os.environ.get("DB_USER","flask_user")
-# DB_PASS = os.environ.get("DB_PASS","flask")
-# #POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
-# INSTANCE_HOST = os.environ.get("INSTANCE_HOST","172.31.32.1")
-# DB_NAME = os.environ.get("DB_NAME","flaskdb")
-# DB_PORT = os.environ.get("DB_PORT","5433")
-
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
-#POSTGRES_URL = os.environ.get("POSTGRES_URL","localhost:5433")
 INSTANCE_HOST = os.environ.get("INSTANCE_HOST")
 DB_NAME = os.environ.get("DB_NAME")
 DB_PORT = os.environ.get("DB_PORT")
